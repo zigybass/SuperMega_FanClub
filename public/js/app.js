afa9f4c8f0910a02c3a7c143f46acb0d62da40e2
Zigy <zigybass@gmail.com>
2019-08-30 11:24:56 -0400

    newUser is getting to backend. changed email to username. teams are undefined

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     var urlParams = new URLSearchParams(window.location.search);
     const league = urlParams.get("league");
     console.log(league);
@@ -9,4 +10,38 @@ $(document).ready(function(){
     })
     // Output data to your html
 
-})//document.ready
+    // Build New User data for DB
+       let nameInput = $("#nameInput")
+       let userNameInput = $("#usernameInput")
+       let passwordInput = $("#passwordInput")
+       let favNbaInput = $("#favNba")
+        let favNflInput = $("#favNfl")
+        let favMlbInput = $("#favMlb")
+        let favMlsInput = $("#favMls")       
+
+    // Send new User data to backend to store in DB
+    $("#createUser").on("click", function (e) {
+        e.preventDefault();
+        const newUser = {
+            name: nameInput.val().trim(),
+            username: userNameInput.val().trim(),
+            password: passwordInput.val().trim(),
+            favNba: favNbaInput.val(),
+            favNfl: favNflInput.val(),
+            favMlb: favMlbInput.val(),
+            favMls: favMlsInput.val()      
+         }
+        submitUser(newUser);
+        console.log(newUser)
+    })
+
+    function submitUser (user) {
+        $.post("/api/createUser", user, function () {
+               console.log(user)
+        })
+    }
+
+        // console.log($("#nameInput").val().trim(), $("#usernameInput").val().trim(), $("#passwordInput").val().trim(), $("#favNba").val().trim())
+    })
+
+//document.ready
