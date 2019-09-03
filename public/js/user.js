b5d1ae8d14ae8d0b9123ecf147eef9a8f23a98cf
coti_garcia <javiera.garcialatorre@gmail.com>
2019-09-03 13:34:43 -0400

    frontend sports

--- public/js/user.js
+++ public/js/user.js
@@ -2,13 +2,9 @@ $(document).ready(function(){
 
     const urlParams = new URLSearchParams(window.location.search);
     const userID = urlParams.get("id");
-
     console.log(userID);
-
     $.get(`/api/user/${userID}`).then(function(data){
         console.log(data);
     })
-
-
 
 }) // document ready 
