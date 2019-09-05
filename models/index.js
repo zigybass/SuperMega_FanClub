69b098408fd89618b8c898599b6fb8e107d3d9a8
MaxPower <mxmcoursey@gmail.com>
2019-09-04 23:07:39 -0400

    updates to config and index files

--- models/index.js
+++ models/index.js
@@ -8,23 +8,23 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let connection;
+// let connection;
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Jlee1470!!',
-    database: 'db_fan'
-  });
-};
+// if(process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Jlee1470!!',
+//     database: 'db_fan'
+//   });
+// };
 
 fs
   .readdirSync(__dirname)
@@ -45,6 +45,6 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-connection.connect();
+// connection.connect();
 module.exports = db;
-module.exports = connection;
+// module.exports = connection;
