163ce98f62cc740d712dc556eaf2a2c351c27fb8
Zigy <zigybass@gmail.com>
2019-09-05 15:55:08 -0400

    working on passport: un-commented all passport items

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 // Dependencies====================================================================================
 const express = require("express");
 const path = require("path");
-// const passport = require("passport")
-// const session = require("express-session");
-// const LocalStrategy = require("passport-local").Strategy;
+const passport = require("passport")
+const session = require("express-session");
+const LocalStrategy = require("passport-local").Strategy;
 
 
 // Sets up the Express App=========================================================================
 const app = express();
 const PORT = process.env.PORT || 8080;
-// const path = require('path');
 
 
 // Requiring our models for syncing
 const db = require("./models");
 
 // Express Session
-// app.use(session({
-//     secret: "secret",
-//     saveUninitialized: true,
-//     resave: true
-// }))
+app.use(session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true
+}))
 
 // Passport init
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -34,36 +33,36 @@ app.use(express.static("./public"));
 
 
 // //
-// passport.use(new LocalStrategy(
-//     function (username, password, done) {
+passport.use(new LocalStrategy(
+    function (username, password, done) {
 
-//         User.findOne({
-//             where: {
-//                 username: username
-//             }
-//         }).then(user => {
-//             if (user) {
-//                 const valid = user.validatePassword(password);
-//                 console.log("User found");
+        User.findOne({
+            where: {
+                username: username
+            }
+        }).then(user => {
+            if (user) {
+                const valid = user.validatePassword(password);
+                console.log("User found");
 
-//                 if (valid) return done(null, user);
+                if (valid) return done(null, user);
 
-//                 return done(null, false);
-//             } else {
-//                 done(null, false)
-//             }
-//         })
-//     }
-// // ))
+                return done(null, false);
+            } else {
+                done(null, false)
+            }
+        })
+    }
+))
 
-// passport.serializeUser( function (user, done) {
-//     console.log(user);
-//     done(null, user);
-// });
+passport.serializeUser( function (user, done) {
+    console.log(user);
+    done(null, user);
+});
 
-// passport.deserializeUser( function (user, done) {
-//     done(null, user)
-// })
+passport.deserializeUser( function (user, done) {
+    done(null, user)
+})
 
 
 
@@ -71,13 +70,13 @@ app.use(express.static("./public"));
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-// app.post('/login', 
-//     passport.authenticate('local', {
-//         successRedirect: '/',
-//         failureRedirect: '/login',
-//         failureFlash: true
-//     })
-// )
+app.post('/login', 
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    })
+)
 
 
 // Syncing our sequelize models and then starting our Express app=================================
